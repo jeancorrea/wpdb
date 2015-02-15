8622195b7fbfe7bf57d6cab9f29103e2d8ccccd0
Jean Correa <jean.do.violao@gmail.com>
2015-02-15 21:29:03 -0200

    Construindo sidebar parte 2

--- sidebar.php
+++ sidebar.php
@@ -1,5 +1,13 @@
 <div id="pic" class="hide-for-small-only">
-	<img src="wp-content/themes/mytheme/img/bible.png" />
+	<span id="picFundo">
+		<img src="wp-content/themes/mytheme/img/banner.png" />
+		<h2>Blog Prosa de Crente, o blog oficial do BPC, traz pra você artigos, imagens, vídeos e vários conteúdos pra compartilhar a Palavra de Deus e o Evangelho de Jesus Cristo.</h2>
+		<span id="social">
+			<a href="http://www.facebook.com/BlogProsadeCrente" target="_BLANK"><img src="wp-content/themes/mytheme/img/facebook.png" title="Facebook" /></a>
+			<a href="http://twitter.com/prosa_crente" target="_BLANK"><img src="wp-content/themes/mytheme/img/twitter.png" title="Twitter" /></a>
+			<a href="http://www.prosadecrente.com/feed" target="_BLANK"><img src="wp-content/themes/mytheme/img/rss.png" title="RSS Feed" /></a>
+		</span>
+	</span>
 </div><!--pic-->
 <ul>
 <?php dynamic_sidebar(); ?>
